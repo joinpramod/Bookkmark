98ad836c40095fefba86dfbb20cb582730e0e342
joinpramod <pramodh.suresh@yahoo.com>
2017-11-03 14:18:17 -0400

    Add files via upload

--- Bookkmark/Models/Bookkmark.cs
+++ Bookkmark/Models/Bookkmark.cs
@@ -7,35 +7,36 @@ namespace Bookkmark.Models
 {
     public class Bookkmark
     {
-        public Int64 BookkmarkID { get; set; } 
+        public Int64 BookkmarkID { get; set; }
+        public Int64 ParentID { get; set; }
         public string URL { get; set; } 
         public string Name { get; set; } 
-        public string FolderId { get; set; } 
+        public bool IsFolder { get; set; } 
         public string CreatedUser { get; set; } 
         public string CreatedDateTime { get; set; }
    
     }
 
-    public class Folder
-    {
-        public Int64 FolderID { get; set; }
-        public Int64 ParentID { get; set; }
-        public string Name { get; set; }
-        public string URL { get; set; }
-        public string CreatedUser { get; set; }
-        public string CreatedDateTime { get; set; }
-    }
+    //public class Folder
+    //{
+    //    public Int64 FolderID { get; set; }
+    //    public Int64 ParentID { get; set; }
+    //    public string Name { get; set; }
+    //    public string URL { get; set; }
+    //    public string CreatedUser { get; set; }
+    //    public string CreatedDateTime { get; set; }
+    //}
 
-    public class VwBookkmark
-    {
-        public Int64 BookkmarkID { get; set; }
-        public string URL { get; set; }
-        public string Name { get; set; }
-        public string FolderId { get; set; }
-        public string FolderName { get; set; }
-        public string CreatedUser { get; set; }
-        public string CreatedDateTime { get; set; }
+    //public class VwBookkmark
+    //{
+    //    public Int64 BookkmarkID { get; set; }
+    //    public string URL { get; set; }
+    //    public string Name { get; set; }
+    //    public string FolderId { get; set; }
+    //    public string FolderName { get; set; }
+    //    public string CreatedUser { get; set; }
+    //    public string CreatedDateTime { get; set; }
 
-    }
+    //}
 
 }
