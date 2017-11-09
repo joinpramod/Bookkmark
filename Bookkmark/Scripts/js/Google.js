89825518812b9272abdc34f8ef6017deccf2ac6b
joinpramod <pramodh.suresh@yahoo.com>
2017-11-09 15:20:45 -0500

    Add files via upload

--- Bookkmark/Scripts/js/Google.js
+++ Bookkmark/Scripts/js/Google.js
@@ -58,15 +58,15 @@ function renderButton() {
 
 
 
-window.onbeforeunload = function (e) {
-    try{
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-            console.log('User signed out.');
-        });
-    }
-    catch(err){
-    }
+//window.onbeforeunload = function (e) {
+//    try{
+//        var auth2 = gapi.auth2.getAuthInstance();
+//        auth2.signOut().then(function () {
+//            console.log('User signed out.');
+//        });
+//    }
+//    catch(err){
+//    }
 
 
-};
+//};
