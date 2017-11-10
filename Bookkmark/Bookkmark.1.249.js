aced3579f3db59a180101a1aafad38743aecbe18
joinpramod <pramodh.suresh@yahoo.com>
2017-11-10 14:44:47 -0500

    Add files via upload

--- Bookkmark/Bookkmark.1.249.js
+++ Bookkmark/Bookkmark.1.249.js
@@ -6,7 +6,7 @@
     var ht = querystrArray[1].split("=")[1] + "px";
     var wd = querystrArray[2].split("=")[1] + "px";
     var sd = querystrArray[3].split("=")[1];
-    ifrm.setAttribute("src", "http://localhost:49216/Bookkmark/Show?lnk=" + lnk + "&h=" + ht + "&w=" + wd + "&sd=" + sd + "&t=" + document.title
+    ifrm.setAttribute("src", "http://localhost/Bookkmark/Bookkmark/Show?lnk=" + lnk + "&h=" + ht + "&w=" + wd + "&sd=" + sd + "&t=" + document.title
 );
     ifrm.style.border = "none";
     ifrm.style.overflow = "hidden";
