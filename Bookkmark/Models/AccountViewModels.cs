ef33e021985b8ab833cd16fdbb3d8522170a0391
joinpramod <pramodh.suresh@yahoo.com>
2017-11-22 13:59:15 -0500

    Add files via upload

--- Bookkmark/Models/AccountViewModels.cs
+++ Bookkmark/Models/AccountViewModels.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace Bookkmark.Models
+namespace Bookmark.Models
 {
     public class ExternalLoginConfirmationViewModel
     {
