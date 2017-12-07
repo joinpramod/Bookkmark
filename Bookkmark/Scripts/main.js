1f2def3dfd241d704540e1fd81c9ce2dfb0be3d7
joinpramod <pramodh.suresh@yahoo.com>
2017-12-07 16:58:40 -0500

    Add files via upload

--- Bookkmark/Scripts/main.js
+++ Bookkmark/Scripts/main.js
@@ -222,8 +222,8 @@
       var taskAction = link.getAttribute("data-action");
       var left = (screen.width) - (0.50 * screen.width);
       var top = (screen.height) - (0.60 * screen.height);
-      var height = 305;
-      var width = 500;
+      var height = 540;
+      var width = 480;
       window.open("/bookmark/bookmark/manage?id=" + taskId + "&action=" + taskAction, "_blank", "toolbar=no,scrollbars=no,resizable=no,top=" + top + ",left=" + left + ",width=" + width + ",height=" + height + "");
 
     toggleMenuOff();
