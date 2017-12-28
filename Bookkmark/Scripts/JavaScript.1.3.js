389f110b141deef6e01ac207856727adf113af61
joinpramod <pramodh.suresh@yahoo.com>
2017-12-28 15:31:34 -0500

    Add files via upload

--- Bookkmark/Scripts/JavaScript.1.3.js
+++ Bookkmark/Scripts/JavaScript.1.3.js
@@ -31,97 +31,69 @@ function ValidateUserReg() {
     }
 }
 
-function ValidateEditUser() {
-    var e = document.getElementById("FirstName").value;
-    var b = document.getElementById("LastName").value;
-    var d = document.getElementById("Email").value;
-    if (e == "" || b == "" || d == "") {
-        document.getElementById("divAck").innerText = "Please enter all details. First and Last names, EMail";
-        return false;
-    }
-    else {
-                if (ValidateEMail(d)) {
-                    return true;
-                }
-                else {
-                    document.getElementById("divAck").innerText = "Please enter valid email";
-                    return false;
-            }
-    }
-}
+//function ValidateEditUser() {
+//    var e = document.getElementById("FirstName").value;
+//    var b = document.getElementById("LastName").value;
+//    var d = document.getElementById("Email").value;
+//    if (e == "" || b == "" || d == "") {
+//        document.getElementById("divAck").innerText = "Please enter all details. First and Last names, EMail";
+//        return false;
+//    }
+//    else {
+//                if (ValidateEMail(d)) {
+//                    return true;
+//                }
+//                else {
+//                    document.getElementById("divAck").innerText = "Please enter valid email";
+//                    return false;
+//            }
+//    }
+//}
 
 
-function ValidateSuggestion() {
-    var b = document.getElementById("txtSuggestion").value;
-    var a = document.getElementById("hfUserEMail").value;
-    if (a != "") {
-        if (b == "") {
-            document.getElementById("divAck").innerText = "Please enter all details";
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-    else {
-        document.getElementById("divAck").innerText = "To avoid spams and robots please login to contact us. We appreciate your patience";
-        return false;
-    }
-}
+//function ValidateSuggestion() {
+//    var b = document.getElementById("txtSuggestion").value;
+//    var a = document.getElementById("hfUserEMail").value;
+//    if (a != "") {
+//        if (b == "") {
+//            document.getElementById("divAck").innerText = "Please enter all details";
+//            return false;
+//        }
+//        else {
+//            return true;
+//        }
+//    }
+//    else {
+//        document.getElementById("divAck").innerText = "To avoid spams and robots please login to contact us. We appreciate your patience";
+//        return false;
+//    }
+//}
 
-function ValidateLogin()
-{
-    var a = document.getElementById("txtEMailId").value;
-    var b = document.getElementById("txtPassword").value;
-    if (a != "" && b != "") {
-        if (ValidateEMail(a)) {
-            return true;
-        }
-        else {
-            document.getElementById("divAck").innerText = "Please enter valid email";
-            return false;
-        }
-    }
-    else {
-        document.getElementById("divAck").innerText = "Please enter username and password";
-        return false;
-    }
-}
+//function ValidateLogin()
+//{
+//    var a = document.getElementById("txtEMailId").value;
+//    var b = document.getElementById("txtPassword").value;
+//    if (a != "" && b != "") {
+//        if (ValidateEMail(a)) {
+//            return true;
+//        }
+//        else {
+//            document.getElementById("divAck").innerText = "Please enter valid email";
+//            return false;
+//        }
+//    }
+//    else {
+//        document.getElementById("divAck").innerText = "Please enter username and password";
+//        return false;
+//    }
+//}
 
-function ValidateEMail(a) {
-    var b = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    return b.test(a);
-}
-
-function ValidateForgotPassword() {
-    var a = document.getElementById("txtEMailId").value;
-    if (a != "") {
-        if (ValidateEMail(a)) {
-            return true;
-        }
-        else {
-            document.getElementById("divAck").innerText = "Please enter valid email";
-            return false;
-        }
-    }
-    else {
-        document.getElementById("divAck").innerText = "Please enter email";
-        return false;
-    }
-}
 
 function ValidatePasswords() {
-    var b = document.getElementById("HFOldPassword").value;
     var c = document.getElementById("txtPassword").value;
     var a = document.getElementById("txtNewPassword").value;
     var e = document.getElementById("txtConfirmPassword").value;
     var d = document.getElementById("hfUserEMail").value;
-    if (d != "") {
-        if (b != c) {
-            document.getElementById("divAck").innerText = "Please enter correct Old Password";
-            return false;
-        }
-        else {
             if (a != e) {
                 document.getElementById("divAck").innerText = "Password and ConfirmPassword does not match";
                 return false;
@@ -135,10 +107,28 @@ function ValidatePasswords() {
                     return true;
                 }
             }
-        }
-    }
-    else {
-        document.getElementById("divAck").innerText = "Please login to post";
-        return false;
-    }
 }
+
+
+
+//function ValidateEMail(a) {
+//    var b = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+//    return b.test(a);
+//}
+
+//function ValidateForgotPassword() {
+//    var a = document.getElementById("txtEMailId").value;
+//    if (a != "") {
+//        if (ValidateEMail(a)) {
+//            return true;
+//        }
+//        else {
+//            document.getElementById("divAck").innerText = "Please enter valid email";
+//            return false;
+//        }
+//    }
+//    else {
+//        document.getElementById("divAck").innerText = "Please enter email";
+//        return false;
+//    }
+//}
