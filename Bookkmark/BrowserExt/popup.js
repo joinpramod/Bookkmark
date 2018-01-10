e294baceabedd21e6ad941ec9d350d870acec473
joinpramod <pramodh.suresh@yahoo.com>
2018-01-10 09:55:54 -0500

    Add files via upload

--- Bookkmark/BrowserExt/popup.js
+++ Bookkmark/BrowserExt/popup.js
@@ -7,31 +7,9 @@ document.getElementById("clickme").addEventListener("click", function(e) {
 chrome.runtime.onMessage.addListener(function (message, sender) {
     //document.getElementById("divTitle").innerText = message.title;
     //document.getElementById("divURL").innerText = message.url;
-    var BookmarkModel = {
-        'Bookmark':message.url,
-        'title': message.title,
-        'ipAddr': '',
-       'bookmarkImgSrc':'extension'
-    };
-    $.ajax({
-        url: 'http://localhost/bookmark/bookmark/addbm',
-        type: 'POST',
-        dataType: "text",
-        data: BookmarkModel,
-        success: function (data) {
-            if (data == null || data == "") {
-                //alert("Bookmark not added");
-                var left = (screen.width) - (0.30 * screen.width);
-                var top = (screen.height) - (0.80 * screen.height);
-                var height = screen.height * 0.50;
-                var width = screen.height * 0.38;
-                window.open("http://localhost/bookmark/account/login", "_blank", "toolbar=yes,scrollbars=no,resizable=no,top=" + top + ",left=" + left + ",width=" + width + ",height=" + height + "");
-            }
-        },
-        error: function (exception) {
-            
-        }
+    var strRedirect = "http://localhost/bookmark/bookmark/addBmrk?url=" + message.url + "&name=" + message.title + "";
+    var creating = browser.tabs.create({
+        url: strRedirect
     });
-
-
+ 
 }); 
