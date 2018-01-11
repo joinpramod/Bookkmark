2e2a3db70fe5a7bdc6eabccee913709b07dfbfe7
joinpramod <pramodh.suresh@yahoo.com>
2018-01-11 00:15:43 -0500

    Add files via upload

--- Bookkmark/BrowserExt/popup.js
+++ Bookkmark/BrowserExt/popup.js
@@ -8,7 +8,7 @@ chrome.runtime.onMessage.addListener(function (message, sender) {
     //document.getElementById("divTitle").innerText = message.title;
     //document.getElementById("divURL").innerText = message.url;
     var strRedirect = "http://booqmarqs.com/bookmark/addBmrk?url=" + message.url + "&name=" + message.title + "";
-    var creating = browser.tabs.create({
+    var creating = chrome.tabs.create({
         url: strRedirect
     });
  
