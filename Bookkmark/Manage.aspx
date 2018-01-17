54ed0bbdebae837c7d1db720b1bc5955f3c38392
joinpramod <pramodh.suresh@yahoo.com>
2018-01-17 15:42:28 -0500

    Add files via upload

---
+++ Bookkmark/Manage.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Manage.aspx.cs" Inherits="Bookmark.Manage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Booqmarq</title>
+    <link id="Link1" runat="server" rel="shortcut icon" href="~/favicon.ico" type="image/x-icon" />
+    <link id="Link2" runat="server" rel="icon" href="~/favicon.ico" type="image/ico" />
+
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="Language" content="en-us" />
+    <meta name="robots" content="noindex" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+            <table style="width:100%;">
+            <tr>
+                <td>
+                    <asp:TextBox ID="txtSQL" runat="server" Height="120px" TextMode="MultiLine" Width="90%"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:GridView ID="GridView1" runat="server" PageSize="100" Width="90%">
+                    </asp:GridView>
+                </td>
+            </tr>
+        </table>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Bookkmark/Manage.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Bookmark
+{
+    public partial class Manage : System.Web.UI.Page
+    {
+        Users user = new Users();
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            user = (Users)Session["User"];
+            //if (user != null && user.Email != null && user.Email == "admin@booqmarqs.com")
+            //{
+                ConnManager connManager = new ConnManager();
+
+                if (txtSQL.Text.ToLower().StartsWith("select"))
+                {
+                    DataTable DSQuestions = new DataTable();
+                    DSQuestions = connManager.GetDataTable(txtSQL.Text);
+                    if (DSQuestions != null)
+                    {
+                        if (DSQuestions.Rows.Count > 0)
+                        {
+                            GridView1.DataSource = DSQuestions;
+                            GridView1.DataBind();
+                        }
+                    }
+                }
+                else
+                {
+                    using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SQLCON"].ToString()))
+                    {
+                        conn.Open();
+                        using (SqlCommand comm = new SqlCommand(txtSQL.Text, conn))
+                        {
+                            comm.ExecuteNonQuery();
+                        }
+                    }                   
+                }
+            //}
+            //else
+            //{
+            //    Response.Redirect("http://www.booqmarqs.com");
+            //}
+        }
+    }
+}
