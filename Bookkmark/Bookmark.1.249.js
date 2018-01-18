94f421374b3d88a59367a7c4ffa8258e57fbc676
joinpramod <pramodh.suresh@yahoo.com>
2018-01-18 16:30:31 -0500

    Add files via upload

--- Bookkmark/Bookmark.1.249.js
+++ Bookkmark/Bookmark.1.249.js
@@ -1,11 +1,17 @@
 ﻿window.onload = function () {
-     var queryString = document.getElementById("bqmrq").src.split("?")[1];
+    var queryString = document.getElementById("bqmrq").src.split("?")[1];
+    var querystrArray = queryString.split("&");
     var ifrm = document.createElement("iframe");
     var lnk = document.referrer;
-    var did = queryString.substring(queryString.indexOf("=") + 1);
+    var ht = querystrArray[0].split("=")[1];
+    var wd = querystrArray[1].split("=")[1];
+    var did = querystrArray[2].substring(4);
     ifrm.setAttribute("src", "http://booqmarqs.com/Bookmark/Show?did=" + did + "&lnk=" + lnk + "&t=" + document.title);
     ifrm.style.border = "none";
     ifrm.style.overflow = "hidden";
-    ifrm.scrolling = "no"
+    ifrm.scrolling = "no";
+    ifrm.id = "frmBqmrq";
+    ifrm.height = (parseInt(ht) + 10).toString() + "px";
+    ifrm.width = (parseInt(wd) + 10).toString() + "px";
     document.getElementById("divBqmrq").appendChild(ifrm);
 };
