32be8270c74a51f5913ca827cee48b766550a3b8
joinpramod <pramodh.suresh@yahoo.com>
2018-01-25 11:25:39 -0500

    Add files via upload

--- Bookkmark/Controllers/BaseController.cs
+++ Bookkmark/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -8,63 +9,90 @@ namespace Bookmark.Controllers
     {
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            if (filterContext.HttpContext.Session["User"] != null)
+            try
             {
-                Users user = (Users)filterContext.HttpContext.Session["User"];
-
-                ViewBag.lblFirstName = user.FirstName;
-                ViewBag.IsUserLoggedIn = true;
-                ViewBag.IsOwner = user.IsOwner;
-                RedirectToProfile(filterContext);
-            }
-            else if (Request.Cookies["BooqmarqsLogin"] != null && Request.Cookies["BooqmarqsLogin"].HasKeys)
-            {
-                string uname = Request.Cookies["BooqmarqsLogin"].Values["EMail"];
-                Users user1 = new Users();
-                user1 = user1.GetUser(uname);
-                Session["User"] = user1;
-                ViewBag.lblFirstName = user1.FirstName;
-
-            }
-            else
-            {
-                if (Request.Url.AbsoluteUri.Contains("bookmark/barcharts")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/barchartshorizon")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/bmadded")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/delete")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/domains")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/editbmfolder")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/import")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/linecharts")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/movebmfolder")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/mybookmarks")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/newbmfolder")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/piecharts")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/reports")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/scriptcode")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("account/changepassword")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("account/edituser")
-                    || Request.Url.AbsoluteUri.ToLower().Contains("account/viewuser")
-                      || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/add")
-                      || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/addbmrk")
-                    )
+                if (filterContext.HttpContext.Session["User"] != null)
                 {
-                    //filterContext.Result = new RedirectResult("Account/Login");
-                    filterContext.Result = new RedirectToRouteResult(
-                                               new RouteValueDictionary
-                                                   {{"controller", "Account"}, {"action", "Login"}});
-                    return;
-                    
+                    Users user = (Users)filterContext.HttpContext.Session["User"];
+
+                    ViewBag.lblFirstName = user.FirstName;
+                    ViewBag.IsUserLoggedIn = true;
+                    ViewBag.IsOwner = user.IsOwner;
+                    RedirectToProfile(filterContext);
                 }
-
-                if (Request.Url.AbsoluteUri.ToLower().Contains("bookmark/extbookmarks"))
+                else if (Request.Cookies["BooqmarqsLogin"] != null && Request.Cookies["BooqmarqsLogin"].HasKeys)
                 {
-                    filterContext.Result = new RedirectToRouteResult(
-                                               new RouteValueDictionary
-                                                   {{"controller", "Account"}, {"action", "ExtLogin"}});
-                    return;
+                    string uname = Request.Cookies["BooqmarqsLogin"].Values["EMail"];
+                    Users user1 = new Users();
+                    user1 = user1.GetUser(uname);
+                    Session["User"] = user1;
+                    ViewBag.lblFirstName = user1.FirstName;
 
                 }
+                else
+                {
+                    if (Request.Url.AbsoluteUri.Contains("bookmark/barcharts")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/barchartshorizon")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/bmadded")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/delete")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/domains")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/editbmfolder")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/import")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/linecharts")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/movebmfolder")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/mybookmarks")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/newbmfolder")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/piecharts")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/reports")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/scriptcode")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("account/changepassword")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("account/edituser")
+                        || Request.Url.AbsoluteUri.ToLower().Contains("account/viewuser")
+                          || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/add")
+                          || Request.Url.AbsoluteUri.ToLower().Contains("bookmark/addbmrk")
+                        )
+                    {
+                        //filterContext.Result = new RedirectResult("Account/Login");
+                        filterContext.Result = new RedirectToRouteResult(
+                                                   new RouteValueDictionary
+                                                       {{"controller", "Account"}, {"action", "Login"}});
+                        return;
+
+                    }
+
+                    if (Request.Url.AbsoluteUri.ToLower().Contains("bookmark/extbookmarks"))
+                    {
+                        filterContext.Result = new RedirectToRouteResult(
+                                                   new RouteValueDictionary
+                                                       {{"controller", "Account"}, {"action", "ExtLogin"}});
+                        return;
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Session["User"] = null;
+                Session["Facebook"] = null;
+                Session["bookmark"] = null;
+                Session.RemoveAll();
+
+                if (Request.Cookies["BooqmarqsLogin"] != null)
+                {
+                    HttpCookie myCookie = Request.Cookies["BooqmarqsLogin"];
+                    myCookie.Expires = DateTime.Now.AddDays(-1d);
+                    myCookie.Values["UserId"] = null;
+                    myCookie.Values["FirstName"] = null;
+                    myCookie.Values["LastName"] = null;
+                    Response.Cookies.Set(myCookie);
+                }
+
+
+                filterContext.Result = new RedirectToRouteResult(
+                                                   new RouteValueDictionary
+                                                       {{"controller", "Home"}, {"action", "Index"}});
+                return;
             }
         }
 
